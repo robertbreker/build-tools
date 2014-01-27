0c9076ab1bc3e16de66808b61ef951af2f31598a
Robert Breker <robert.breker@citrix.com>
2014-01-27 16:57:47 +0000

    Initial code commit
    Signed-off-by: Robert Breker <robert.breker@citrix.com>

---
+++ install-centos.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -eux
+
+if [ "$#" -ne 4 ]; then
+        echo "Invalid parameters"
+        echo "Usage: $0 xenserver_host xenserver_password install_password install_repository"
+        exit 1
+fi
+
+xenserver_host="$1"
+xenserver_password="$2"
+install_password="$3"
+install_repository="$4"
+
+function xecommand() {
+    xe -s $xenserver_host -u $xenserver_username -pw $xenserver_password "$@"
+}
+
+xenserver_username="root"
+vm_name="MyCentOs"
+template_name="CentOS 6 (64-bit)"
+kickstart_file="anaconda-ks.cfg"
+bridge="xenbr0"
+
+vm_uuid=$(xecommand vm-install new-name-label="$vm_name" template="$template_name" --minimal)
+vbd_uuid=$(xecommand vbd-list vm-uuid="$vm_uuid" --minimal)
+vdi_uuid=$(xecommand vbd-param-get param-name=vdi-uuid uuid="$vbd_uuid" --minimal)
+xecommand vdi-resize uuid="$vdi_uuid" disk-size=15GiB
+
+old_pv_args=$(xecommand vm-param-get uuid="$vm_uuid" param-name=PV-args --minimal)
+temp_kickstart_path=$(mktemp)
+temp_kickstart_filename=$(basename "$temp_kickstart_path")
+cp "$kickstart_file" "$temp_kickstart_path"
+escaped_install_repository=$(echo "$install_repository" | sed -e 's/[\/&]/\\&/g')
+sed -i  -e "s/<install_repository>/$escaped_install_repository/g" \
+        -e "s/<install_password>/$install_password/g" \
+        "$temp_kickstart_path"
+sshpass -p "$xenserver_password" scp -o StrictHostKeyChecking=no "$temp_kickstart_path" "$xenserver_username"@"$xenserver_host":/opt/xensource/www/
+rm -f "$temp_kickstart_path"
+new_pv_args="$old_pv_args ks=http://$xenserver_host/$temp_kickstart_filename"
+xecommand vm-param-set uuid="$vm_uuid"\
+    other-config:install-methods=http \
+    other-config:install-repository="$install_repository" \
+    PV-args="$new_pv_args"
+network_uuid=$(xecommand network-list bridge="$bridge" --minimal)
+xecommand vif-create vm-uuid="$vm_uuid" network-uuid="$network_uuid" device="0"
+xecommand vm-cd-add uuid="$vm_uuid" device=1 cd-name="xs-tools.iso"
+xecommand vm-start vm="$vm_uuid"
+
+while [[ "$vm_uuid" == $(xecommand vm-list uuid="$vm_uuid" power-state=running --minimal) ]]; do
+    sleep 3
+done
+
+sshpass -p "$xenserver_password" ssh -o StrictHostKeyChecking=no "$xenserver_username"@"$xenserver_host" "rm /opt/xensource/www/$temp_kickstart_filename"
+xecommand vm-cd-eject vm="$vm_uuid"
+
+echo $vm_uuid
+exit 0
---
+++ install-cloudstack.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+set -eux
+
+if [ "$#" -ne 2 ]; then
+        echo "Invalid parameters"
+        echo "Usage: $0 ccp systemvm"
+        exit 1
+fi
+
+ccp=$1
+systemvm=$2
+
+vhd_util="http://download.cloud.com.s3.amazonaws.com/tools/vhd-util"
+
+yum -y install ntp
+
+wget "$ccp"
+tar zxf ${ccp%%/*}
+cd ${ccp%.tar.gz}
+./install.sh -m
+
+wget "$vhd_util"
+mv vhd-util /usr/share/cloudstack-common/scripts/vm/hypervisor/xenserver/
+
+yum -y install mysql-server mysql
+sed -i "s/\[mysqld\]/\[mysqld\]\ninnodb_rollback_on_timeout=1\ninnodb_lock_wait_timeout=600\nmax_connections=350\nlog-bin=mysql-bin\nbinlog-format = \'ROW\'/g" /etc/my.cnf
+chkconfig mysqld on
+service mysqld restart
+mysql -u root <<EOF
+UPDATE mysql.user SET Password=PASSWORD('cloud') WHERE User='root';
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', 'localhost.localnet', '127.0.0.1', '::1');
+DROP DATABASE test;
+FLUSH PRIVILEGES;
+EOF
+setenforce Permissive
+
+yum -y install nfs-utils
+chkconfig rpcbind on
+mkdir -p /export/primary
+mkdir -p /export/secondary
+echo "/export  *(rw,async,no_root_squash,no_subtree_check)" >> /etc/exports
+exportfs -a
+sed -i -e "/LOCKD_TCPPORT=32803/ s/# *//"\
+       -e "/LOCKD_UDPPORT=32769/ s/# *//"\
+       -e "/MOUNTD_PORT=892/ s/# *//"\
+       -e "/RQUOTAD_PORT/ s/# *//"\
+       -e "/STATD_PORT/ s/# *//"\
+       -e "/STATD_OUTGOING_PORT/ s/# *//"\
+       /etc/sysconfig/nfs
+
+iptables -A INPUT -m state --state NEW -p udp --dport 111 -j ACCEPT
+iptables -A INPUT -m state --state NEW -p tcp --dport 111 -j ACCEPT
+iptables -A INPUT -m state --state NEW -p tcp --dport 2049 -j ACCEPT
+iptables -A INPUT -m state --state NEW -p tcp --dport 32803 -j ACCEPT
+iptables -A INPUT -m state --state NEW -p udp --dport 32769 -j ACCEPT
+iptables -A INPUT -m state --state NEW -p tcp --dport 892 -j ACCEPT
+iptables -A INPUT -m state --state NEW -p udp --dport 892 -j ACCEPT
+iptables -A INPUT -m state --state NEW -p tcp --dport 875 -j ACCEPT
+iptables -A INPUT -m state --state NEW -p udp --dport 875 -j ACCEPT
+iptables -A INPUT -m state --state NEW -p tcp --dport 662 -j ACCEPT
+iptables -A INPUT -m state --state NEW -p udp --dport 662 -j ACCEPT
+service iptables save
+
+cloudstack-setup-databases cloud:cloud@localhost --deploy-as=root:cloud
+cloudstack-setup-management
+
+/usr/share/cloudstack-common/scripts/storage/secondary/cloud-install-sys-tmplt -m /export/secondary -u $systemvm -h xenserver
